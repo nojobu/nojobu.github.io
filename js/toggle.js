9d023427fa123dea544b83081b988891398c3bdd
nora <s543411@htw-berlin.de>
2019-04-15 16:19:29 +0200

    js toggle

--- js/toggle.js
+++ js/toggle.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-		$('.toggle').click(function(){
-			$('.toggle').toggleClass('active')
-		})
-})
+/*$('.toggle').click(function(){
+$('.toggle').toggleClass('active')
+})*/
+
+let mainNav = document.getElementById('js-toggle-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+
+navBarToggle.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+    navBarToggle.classList.toggle('toggle-active');
+});
+
+mainNav.addEventListener('click', function () {
+    mainNav.classList.toggle('active');
+    navBarToggle.classList.toggle('toggle-active');
+}); 
+        
+        
+        
+});
