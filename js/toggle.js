4d8dc20509776a41b28b5ce2eb28bc3ba911e8b2
nora <s543411@htw-berlin.de>
2019-10-17 23:27:52 +0200

    Update toggle.js

--- js/toggle.js
+++ js/toggle.js
@@ -13,22 +13,18 @@ navBarToggle.addEventListener('click', function () {
     mainNav.classList.toggle('active');
     navBarToggle.classList.toggle('toggle-active');
 });
-navBarToggle.addEventListener('touchstart', function () {
-    mainNav.classList.toggle('active');
-    navBarToggle.classList.toggle('toggle-active');
-});
 
 mainNav.addEventListener('click', function () {
     mainNav.classList.toggle('active');
     navBarToggle.classList.toggle('toggle-active');
 }); 
-                                                              //a[href*="#"]:not([href="#"])
+
+   //a[href*="#"]:not([href="#"])
    /* $('.main-toggle-menu .main-toggle-menu-link> a.nav-links-toggle:not(a[href="#"])').on('click', function() {
-    self.location = $(this).attr('href'); */
-    $('.main-toggle-menu .main-toggle-menu-link> a[href*="#"]:not([href="#"])').on('click touchstart', function() {
-    self.location = $(this).attr('href');
+    self.location = $(this).attr('href'); 
+    $('.main-toggle-menu .main-toggle-menu-link> a[href*="#"]:not([href="#"])').on('click', function() {
         
-});
+});*/
     
 });
 
